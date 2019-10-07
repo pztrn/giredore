d7d3359ac61bfbe63dd7af9659a6e586f1aaafb6
Stanislav N. aka pztrn <pztrn@pztrn.name>
2019-10-07 22:33:05 +0500

    Removed ToDos as they're transformed into issues.

--- internal/requester/httpclient.go
+++ internal/requester/httpclient.go
@@ -10,21 +10,14 @@ import (
 func getHTTPClient() *http.Client {
 	c := &http.Client{
 		Transport: &http.Transport{
-			// ToDo: configurable.
 			ExpectContinueTimeout: time.Second * 5,
 			DialContext: (&net.Dialer{
-				// ToDo: configurable.
 				Timeout: time.Second * 5,
 			}).DialContext,
-			// ToDo: configurable.
 			ResponseHeaderTimeout: time.Second * 5,
-			// ToDo: configurable.
-			TLSHandshakeTimeout: time.Second * 10,
+			TLSHandshakeTimeout:   time.Second * 10,
 		},
 	}
-
-	// ToDo: skip verifying insecure certificates if option was
-	// specified.
 
 	return c
 }
